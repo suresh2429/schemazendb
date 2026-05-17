12f72aba8dad380b3551f96629b57081a2a3bf79
suresh2429 <bsuresh2429@gmail.com>
2026-05-17 15:54:19 +0530

    Auto-generated commit

---
+++ browser/chunk-KIJK45AO.js
@@ -0,0 +1,1 @@
+import{n as m,s}from"./chunk-RGBWYOJ5.js";import{Lb as a,ga as o,nb as n}from"./chunk-SM5APWOK.js";var c=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-schema-db"]],standalone:!0,features:[a],decls:1,vars:0,template:function(e,p){e&1&&n(0,"router-outlet")},dependencies:[m,s]})};export{c as SchemaDbComponent};
